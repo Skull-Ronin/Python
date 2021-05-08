c03385f0a6a42ff0ffaaf01ff3597dc9eb7c7442
Skull-Ronin <62279568+Skull-Ronin@users.noreply.github.com>
2021-05-08 02:04:14 -0300

    realizando ejercicios
    aprendiendo python

---
+++ mi app.py
@@ -0,0 +1,9 @@
+print('Hola soy Edgardo ;D')
+
+input ('Cuantos Años Tienes?')
+input ('Como te llamas')
+input ('Trabajas o Estudias')
+input ('Cual es tu color Favorito')
+
+print = input,input,input,input
+
