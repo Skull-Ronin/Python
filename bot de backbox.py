7e7f9fa2dd38f7e93e5a5327fc677845d224efd8
Skull-Ronin <62279568+Skull-Ronin@users.noreply.github.com>
2021-05-08 02:12:55 -0300

    este es un bot de ig
    le  falta que le de me gusta a las fotos y comente fotos

---
+++ bot de backbox.py
@@ -0,0 +1,11 @@
+# importar la libreta a utilizar
+from instabot import Bot
+
+# crear el nuevo objeto
+mi_bot = Bot()
+
+# Nombre de cuenta y contraseña 
+mi_bot.login (username ='blackbox.vgg', password= que te importa ')
+
+#cuenta a la que me pego para los seguidores
+mi_bot.follow_followers('cuenta a la que uso para escalar  ')
